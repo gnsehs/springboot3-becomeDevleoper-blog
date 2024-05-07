6d7de43a129dd17cadf842a53f37e2cfd57a3016
DoHun Han <sasuke12362@gmail.com>
2024-05-07 22:21:12 +0900

    02: 스프링 부트 3 시작하기

--- src/main/java/me/shinsunyoung/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootDeveloperApplication {
+public class SpringBootDeveloperApplication { // 자바의 main() 같은 역할 즉 여기서 스프링 부트 시작
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
     }
