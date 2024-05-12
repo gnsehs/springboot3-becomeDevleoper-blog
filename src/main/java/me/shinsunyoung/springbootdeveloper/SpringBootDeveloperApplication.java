765a5651caeab2d3d8b801b3e3c0fa2ca213ec84
DoHun Han <sasuke12362@gmail.com>
2024-05-13 02:59:35 +0900

    04: 스프링 부트 3와 테스트

--- src/main/java/me/shinsunyoung/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -3,7 +3,7 @@ package me.shinsunyoung.springbootdeveloper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // 메인 애플리케이션 클래스에 추가하는 애너테이션
 public class SpringBootDeveloperApplication { // 자바의 main() 같은 역할 즉 여기서 스프링 부트 시작
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
